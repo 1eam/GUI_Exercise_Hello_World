497ed4790313a43145f7c16839de5a6706c69103
Esther <esther191299@gmail.com>
2019-09-22 11:20:42 +0200

    Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,34 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
+	public static void main(String... args) {
+		launch(args);
+	}
+	@Override
+	public void start(Stage primaryStage) {
+		Text txt = new Text("Hello, world!");
+		txt.relocate(135, 40);
+
+		Button btn = new Button("Exit");
+		btn.relocate(155, 80);
+		btn.setOnAction(e:> {
+			System.out.println("Bye! See you later!");
+			Platform.exit();
+		});
+
+		Pane pane = new Pane();
+		pane.getChildren().addAll(txt, btn);
+
+		primaryStage.setTitle("The primary stage (top-level container)");
+		primaryStage.onCloseRequestProperty()
+		.setValue(e:> System.out.println("Bye! See you later!"));
+		primaryStage.setScene(new Scene(pane, 350, 150));
+		primaryStage.show();
+	}
+}
